21400acb9f6209a213009dae48dc8497658ac387
tomanick36018 <tomanick36018@gmail.com>
2026-07-13 08:51:42 +0200

    Update create_daily_summary.py

--- scripts/create_daily_summary.py
+++ scripts/create_daily_summary.py
@@ -34,21 +34,25 @@ for activity in activities:
     day["sessions"] += 1
     day["training_load"] += activity.get("icu_training_load", 0)
     day["hours"] += activity.get("moving_time", 0) / 3600
-    day["distance_km"] += activity.get("distance", 0) / 1000
+    distance = activity.get("distance") or 0
+    day["distance_km"] += distance / 1000
 
     activity_type = activity.get("type", "").lower()
 
     if "run" in activity_type:
         day["running"]["sessions"] += 1
-        day["running"]["distance_km"] += activity.get("distance", 0) / 1000
+        distance = activity.get("distance") or 0
+        day["running"]["distance_km"] += distance / 1000
 
     elif "ebike" in activity_type:
         day["cycling_commute"]["sessions"] += 1
-        day["cycling_commute"]["distance_km"] += activity.get("distance", 0) / 1000
+        distance = activity.get("distance") or 0
+        day["cycling_commute"]["distance_km"] += distance / 1000
 
     elif "ride" in activity_type or "bike" in activity_type:
         day["cycling_performance"]["sessions"] += 1
-        day["cycling_performance"]["distance_km"] += activity.get("distance", 0) / 1000
+        distance = activity.get("distance") or 0
+        day["cycling_performance"]["distance_km"] += distance / 1000
 
     # Eenvoudige definitie van een kwaliteitssessie
     if activity.get("icu_training_load", 0) >= 60:
